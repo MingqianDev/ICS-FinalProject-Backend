8c70450ea518eaebc6d0e1c10f09146378a6888a
kongling7 <1caokyl@hdsb.ca>
2023-06-03 17:28:32 -0400

    server

---
+++ globals.js
@@ -0,0 +1,23 @@
+// Define an object to hold the global variables
+const globals = {
+    longitude: 0,
+    latitude: 0
+}
+
+// Define a function to set a global variable
+function setGlobal(name, value) {
+    //console.log(`Setting global variable '${name}' to '${value}'`);
+    globals[name] = value;
+}
+
+// Define a function to get a global variable
+function getGlobal(name) {
+    // console.log(`Getting global variable '${name}': '${JSON.stringify(globals[name], null, 2)}'`);
+    return globals[name];
+}
+
+// Export the functions
+module.exports = {
+    setGlobal,
+    getGlobal
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*', // You can restrict this to specific domains if needed.
+        methods: ['GET', 'POST']
+    }
+});
+const PORT = process.env.PORT || 3000;
+
+const globals = require('./globals.js');
+
+io.on('connection', (socket) => {
+    socket.on('locationData', (data)=>{
+        globals.setGlobal('longitude', data.longitude);
+        globals.setGlobal('latitude', data.latitude);
+        console.log(data.longitude, data.latitude);
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
