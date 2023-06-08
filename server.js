d80a1317b3641ab0f61c47dc0f17ec4c18cea081
kongling7 <1caokyl@hdsb.ca>
2023-06-08 07:40:46 -0400

    express server

--- server.js
+++ server.js
@@ -1,32 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*', // You can restrict this to specific domains if needed.
-        methods: ['GET', 'POST']
-    }
-});
-const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const https = require('https');
+// const server = http.createServer(app);
+const server = https.createServer({
+    key: fs.readFileSync('weatherpro.top-ssl-bundle/private.key.pem'),
+    cert: fs.readFileSync('weatherpro.top-ssl-bundle/OriginCertificate.pem')
+}, app);
+
+const PORT = 4000;
 
 const globals = require('./globals.js');
 const getWeatherData = require('./WeatherData/getCurrentWeatherData.js');
 const getCity = require('./WeatherData/getCity.js');
 const getDailyWeather = require('./WeatherData/getDailyWeatherData.js');
-io.on('connection', (socket) => {
-    socket.on('locationData', (data)=>{
-        globals.setGlobal('longitude', data.longitude);
-        globals.setGlobal('latitude', data.latitude);
-        console.log(data.latitude, data.longitude);
-        getWeatherData(socket);
-        getCity(socket);
-    });
 
-    socket.on('getWeatherData', ()=>{
-        getDailyWeather(socket);
-    })
-})
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log(latitude, longitude);
+
+    if (!latitude || !longitude) {
+        return res.status(400).json({ error: 'Missing latitude or longitude' });
+    }
+
+    globals.setGlobal('longitude', longitude);
+    globals.setGlobal('latitude', latitude);
+
+    const city = await getCity(latitude, longitude);
+    const currentWeather = await getWeatherData();
+    const dailyWeather = await getDailyWeather();
+
+    res.json({
+        city,
+        currentWeather,
+        dailyWeather
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
